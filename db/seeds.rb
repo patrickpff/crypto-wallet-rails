30ae71577495adf32eb7e192663ffa4ae048e002
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2026-01-18 18:43:48 -0300

    seeding coins

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Seeding Coins..."
+
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  url_image: "https://cryptologos.cc/logos/bitcoin-btc-logo.png"
+)
+
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETH",
+  url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"
+)
+
+Coin.create!(
+  description: "Dash",
+  acronym: "DASH",
+  url_image: "https://cryptologos.cc/logos/dash-dash-logo.png"
+)
+
+Coin.create!(
+  description: "Litecoin",
+  acronym: "LTC",
+  url_image: "https://cryptologos.cc/logos/litecoin-ltc-logo.png"
+)
+
+Coin.create!(
+  description: "Ripple",
+  acronym: "XRP",
+  url_image: "https://cryptologos.cc/logos/xrp-xrp-logo.png"
+)
+
+Coin.create!(
+  description: "Binance Coin",
+  acronym: "BNB",
+  url_image: "https://cryptologos.cc/logos/bnb-bnb-logo.png"
+)
