0ec094000be932073e0ee7998a408afc9190dc3a
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2026-01-19 11:17:10 -0300

    adding Mining type to coins form

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_types_options, only: %i[ new edit create update ]
 
   # GET /coins or /coins.json
   def index
@@ -58,6 +59,11 @@ class CoinsController < ApplicationController
   end
 
   private
+    # Set mining types options for forms
+    def set_mining_types_options
+      @mining_types_options = MiningType.all.pluck(:description, :id)
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params.expect(:id))
@@ -65,6 +71,6 @@ class CoinsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def coin_params
-      params.expect(coin: [ :description, :acronym, :url_image ])
+      params.expect(coin: [ :description, :acronym, :url_image, :mining_type_id ])
     end
 end
